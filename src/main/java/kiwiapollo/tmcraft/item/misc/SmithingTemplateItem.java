13658aef916b595860bb2030694a32703d5d0cf9
fractalbarista <fractalbarista@gmail.com>
2025-01-04 14:40:07 +0900

    Edited to use built-in SmithingTemplateItem

--- src/main/java/kiwiapollo/tmcraft/item/misc/SmithingTemplateItem.java
+++
@@ -1,9 +0,0 @@
-package kiwiapollo.tmcraft.item.misc;
-
-import net.minecraft.item.Item;
-
-public class SmithingTemplateItem extends Item {
-    public SmithingTemplateItem() {
-        super(new Item.Settings());
-    }
-}
--- src/main/java/kiwiapollo/tmcraft/item/misc/SmithingTemplateItems.java
+++ src/main/java/kiwiapollo/tmcraft/item/misc/SmithingTemplateItems.java
@@ -1,10 +1,31 @@
 package kiwiapollo.tmcraft.item.misc;
 
 import kiwiapollo.tmcraft.TMCraft;
+import net.minecraft.item.SmithingTemplateItem;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
+
 public enum SmithingTemplateItems {
-    MOVE_UPGRADE_SMITHING_TEMPLATE("move_upgrade_smithing_template", new SmithingTemplateItem());
+    MOVE_UPGRADE_SMITHING_TEMPLATE("move_upgrade_smithing_template", new SmithingTemplateItem(
+            Text.translatable("item.tmcraft.move_upgrade_smithing_template.applies_to").formatted(Formatting.BLUE),
+            Text.translatable("item.tmcraft.move_upgrade_smithing_template.ingredients").formatted(Formatting.BLUE),
+            Text.translatable("item.tmcraft.move_upgrade_smithing_template.title").formatted(Formatting.GRAY),
+            Text.translatable("item.tmcraft.move_upgrade_smithing_template.base_slot_description"),
+            Text.translatable("item.tmcraft.move_upgrade_smithing_template.additions_slot_description"),
+            List.of(
+                    Identifier.of(TMCraft.MOD_ID, "item/empty_slot_disc"),
+                    Identifier.of(TMCraft.MOD_ID, "item/empty_slot_egg"),
+                    Identifier.of(TMCraft.MOD_ID, "item/empty_slot_book")
+            ),
+            List.of(
+                    Identifier.of(Identifier.DEFAULT_NAMESPACE, "item/empty_slot_ingot"),
+                    Identifier.of(Identifier.DEFAULT_NAMESPACE, "item/empty_slot_emerald"),
+                    Identifier.of(Identifier.DEFAULT_NAMESPACE, "item/empty_slot_diamond")
+            )
+    ));
 
     private final Identifier identifier;
     private final SmithingTemplateItem item;
